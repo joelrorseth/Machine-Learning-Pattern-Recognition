c29e4dfc5051d147dcdb018961bb28fd13c9a3e2
Joel Rorseth <joelrorseth@gmail.com>
2017-10-28 17:10:52 -0400

    Setup for Assignment 2

---
+++ Assignment2/question2.py
@@ -0,0 +1,17 @@
+from sklearn import svm
+
+import numpy as np
+import pandas as pd
+
+
+# SVM Classifier using linear kernel
+def svm_linear:
+    #TODO
+
+# SVM Classifier using deg. 2 polynomial kernel
+def svm_polynomial:
+    #TODO
+
+# SVM Classifier using RBF
+def svm_rbf:
+    # TODO
